cc8ea60dc220e90e9f8aa4dfa96656f9ae8bc36f
jamunashri <jamunashrishanmugasundaram@gmail.com>
2022-08-08 15:16:40 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BackgroundDetector } from './BackgroundDetector';
+import Footer from './Footer'
 
 function App() {
   return (
     <><h1 className='title'> Background color Hiding </h1>
     <BackgroundDetector />
+    <Footer />
     </>
   );
 }
